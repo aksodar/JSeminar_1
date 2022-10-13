b4a8bff562c92f7790a9fc2099e78a941c9300ca
aksdar31415@gmail.com <Zx38745!!!!!>
2022-10-13 18:21:38 +0300

    Initial commit

---
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -0,0 +1,14 @@
+package ru.gb.jseminar;
+
+public class Task3 {
+
+    //Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.
+    public static void main(String[] args) {
+
+    }
+
+    public int count(double[] array){
+
+        return 0;
+    }
+}
