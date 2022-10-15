358b6af644a572429852f8cfdcce9573d3db4dd6
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-10-15 20:53:48 +0300

    small changes in Task2

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -23,7 +23,7 @@ public class Task2 {
 
 		if (hour >= 5 && hour < 12)			System.out.println("Доброе утро, " + name);
 		else if (hour >= 12 && hour < 18)	System.out.println("Добрый день, " + name);
-		else if (hour > 18 && hour < 23)	System.out.println("Добрый вечер, " + name);
+		else if (hour >= 18 && hour < 23)	System.out.println("Добрый вечер, " + name);
 		else								System.out.println("Доброй ночи, " + name);
 	}
 }
