3b6cbc3c33362cea83b17aa4f8eb7f39d59fb650
Vladimir_Artamonov <tomodachiva@gmail.com>
2022-10-16 20:46:08 +0300

    Task 2_Class work

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,14 +1,43 @@
 package ru.gb.jseminar;
 
+import java.sql.Array;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.Date;
+
 public class Task2 {
 
-    //В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида
-    //"Доброе утро, <Имя>!", если время от 05:00 до 11:59
-    //"Добрый день, <Имя>!", если время от 12:00 до 17:59;
-    //"Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
-    //"Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+    // В консоли запросить имя пользователя. В зависимости от текущего времени,
+    // вывести приветствие вида
+    // "Доброе утро, <Имя>!", если время от 05:00 до 11:59
+    // "Добрый день, <Имя>!", если время от 12:00 до 17:59;
+    // "Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+    // "Доброй ночи, <Имя>!", если время от 23:00 до 4:59
     public static void main(String[] args) {
+        String name = input_name();
+        LocalDateTime ldt = LocalDateTime.now();
+        int now = ldt.getHour();
 
+        if (now < 12 && now > 5) {
+            System.out.printf("Доброе утро %s!\n", name);
+        } else if (now > 12 && now < 18) {
+            System.out.printf("Добрый день %s!\n", name);
+        } else if (now > 18 && now < 23) {
+            System.out.printf("Добрый вечер %s!\n", name);
+        } else {
+            System.out.printf("Доброй ночи %s!\n", name);
+        }
+
+    }
+
+    public static String input_name() {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("name: ");
+        String name = iScanner.nextLine();
+        iScanner.close();
+        return name;
     }
 
 }
