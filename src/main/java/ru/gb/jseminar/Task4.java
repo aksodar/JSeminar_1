5c1a44924f12e299f5f409104637b4dbd0363ac0
IlyaFilippov <ilyafil0608@gmail.com>
2022-10-18 00:24:43 +0300

    Task4 done

--- src/main/java/ru/gb/jseminar/Task4.java
+++ src/main/java/ru/gb/jseminar/Task4.java
@@ -1,5 +1,7 @@
 package ru.gb.jseminar;
 
+import java.util.Scanner;
+
 public class Task4 {
 
     //Реализовать функцию возведения числа а в степень b. a, b – целые числа.
@@ -8,10 +10,17 @@ public class Task4 {
     //Пример 2: а = 2, b = -2, ответ: 0.25
     //Пример 3: а = 3, b = 0, ответ: 1
     public static void main(String[] args) {
-
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("a: ");
+        double a = iScanner.nextDouble();
+        System.out.printf("b: ");
+        double b = iScanner.nextDouble();
+        iScanner.close();
+        double res = exponentiation(a, b);
+        System.out.println(res);
     }
 
-    public double exponentiation(int a, int b){
-        return 0;
+    public static double exponentiation(double a, double b){
+        return Math.pow(a, b);
     }
 }
