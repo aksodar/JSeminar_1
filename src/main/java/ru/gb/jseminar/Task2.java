1a570c2e558e256759e72da175f72ff6740d5164
IlyaFilippov <ilyafil0608@gmail.com>
2022-10-17 22:24:37 +0300

    task2 done

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,5 +1,11 @@
 package ru.gb.jseminar;
 
+import java.sql.Time;
+import java.time.DateTimeException;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Task2 {
 
     //В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида
@@ -8,7 +14,20 @@ public class Task2 {
     //"Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
     //"Доброй ночи, <Имя>!", если время от 23:00 до 4:59
     public static void main(String[] args) {
-
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите имя: ");
+        String name = iScanner.nextLine();
+        LocalTime time = LocalTime.now();
+        int hour = time.getHour();
+        if (hour >= 5 && hour < 12) {
+            System.out.printf("Доброе утро, %s", name);
+        } else if (hour >= 12 && hour < 18) {
+            System.out.printf("Добрый день, %s", name);
+        } else if (hour >= 18 && hour < 23) {
+            System.out.printf("Добрый вечер, %s", name);
+        } else {
+            System.out.printf("Доброй ночи, %s", name);
+        }
     }
 
 }
