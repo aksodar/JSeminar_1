762ab9917e5267ad376febe192077d5c41898472
IlyaFilippov <ilyafil0608@gmail.com>
2022-10-17 23:38:57 +0300

    Task3 done

--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -4,11 +4,29 @@ public class Task3 {
 
     //Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.
     public static void main(String[] args) {
-
+        double[] arr = new double[] {1, 1, 0, 0, 0, 1};
+        int result = count(arr);
+        System.out.println(result);
     }
 
-    public int count(double[] array){
+    public static int count(double[] array){
+        int count = 0;
+        int max = 0;
+        for (double item : array) {
+            if(item == 1){
+                count++;
+            } else {
+                if (count > max) {
+                    max = count;
+                    count = 0;
+                }
+            }
+        }
+        if (count < max){
+            return max;
+        } else {
+            return count;
+        }
 
-        return 0;
     }
 }
