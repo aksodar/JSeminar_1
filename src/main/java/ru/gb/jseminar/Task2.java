f3753bfe510d73f0b2693910df68361ff29ddcd4
LeaClover <plesen_14@mail.ru>
2022-10-20 14:17:44 +0300

    edit project task2

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,4 +1,6 @@
 package ru.gb.jseminar;
+import java.util.Scanner;
+import java.time.LocalDateTime;
 
 public class Task2 {
 
@@ -7,8 +9,32 @@ public class Task2 {
     //"Добрый день, <Имя>!", если время от 12:00 до 17:59;
     //"Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
     //"Доброй ночи, <Имя>!", если время от 23:00 до 4:59
-    public static void main(String[] args) {
 
+    LocalDateTime ldt = LocalDateTime.now();
+    Integer currentTime = ldt.getHour();
+
+    public static void main(String[] args) {
+        Task2 task = new Task2();
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите имя: ");
+        String name = iScanner.next();
+        iScanner.close();
+        task.getMSGtime(name);
+        System.out.printf("Текущее время: %s", task.currentTime);
     }
 
+    public void getMSGtime(String name) {
+        if (currentTime >= 5 && currentTime < 11) {
+            System.out.printf("Доброе утро, %s!\n", name);
+            }
+        if (currentTime >= 11 && currentTime < 16) {
+            System.out.printf("Добрый день, %s!\n", name);
+            }
+        if (currentTime >= 16 && currentTime < 23) {
+            System.out.printf("Добрый вечер, %s!\n", name);
+            }
+        if (currentTime >= 23 && currentTime < 5) {
+            System.out.printf("Доброй ночи, %s!\n", name);
+            }
+    }
 }
