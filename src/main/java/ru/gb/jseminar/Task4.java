eb6326769b4fee2c3c263f003d4c3014d1967307
LeaClover <plesen_14@mail.ru>
2022-10-20 14:21:53 +0300

    edit project task4

--- src/main/java/ru/gb/jseminar/Task4.java
+++ src/main/java/ru/gb/jseminar/Task4.java
@@ -1,4 +1,6 @@
 package ru.gb.jseminar;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Task4 {
 
@@ -8,10 +10,22 @@ public class Task4 {
     //Пример 2: а = 2, b = -2, ответ: 0.25
     //Пример 3: а = 3, b = 0, ответ: 1
     public static void main(String[] args) {
-
+        Scanner iScanner = new Scanner(System.in);
+        Task4 hw = new Task4();
+        System.out.printf("Введите число a: ");
+        int a = Integer.parseInt(iScanner.nextLine());
+        System.out.printf("Введите число b: ");
+        double b = Double.parseDouble(iScanner.nextLine());
+        double result = hw.exponentiation(a, b);
+        hw.printResult(a, b, result);
+        iScanner.close();
     }
 
-    public double exponentiation(int a, int b){
-        return 0;
+    public double exponentiation(int a, double b) {
+        return Math.pow(a, b);
+    }
+
+    public void printResult(int a, double b, double result) {
+        System.out.printf("%d ^ %.2f = %.2f", a, b, result);
     }
 }
