a087177d603d28c7718cf2af35cd028fac89af6c
LeaClover <plesen_14@mail.ru>
2022-10-20 14:15:52 +0300

    edit project task1

--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,11 +1,20 @@
 package ru.gb.jseminar;
+import java.util.Scanner;
 
 public class Task1 {
 
     //Написать программу, которая запросит пользователя ввести <Имя> в консоли.
     //Получит введенную строку и выведет в консоль сообщение “Привет, <Имя>!”
     public static void main(String[] args) {
-
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите имя: ");
+        String name = iScanner.nextLine();
+        Task1 task = new Task1();
+        task.getMSG(name);
+        iScanner.close();
     }
 
+    public void getMSG(String name) {
+        System.out.printf("Привет, %s!", name);
+    }
 }
