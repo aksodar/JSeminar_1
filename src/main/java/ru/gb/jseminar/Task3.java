6d4e0aec4be21188d7347f3a659ddd46a6ab6a6e
LeaClover <plesen_14@mail.ru>
2022-10-20 14:19:14 +0300

    edit project task3

--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -4,7 +4,26 @@ public class Task3 {
 
     //Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.
     public static void main(String[] args) {
+        int [] arr = {1, 1, 1, 0, 0, 1, 1, 1, 1, 1};
+        Task3 task = new Task3();
+        int max = task.getMaxCount(arr);
+        System.out.printf("Максимальное количество '1' подряд: %d", max);
+    }
 
+    public int getMaxCount(int [] arr) {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) count++;
+            else {
+                if (count != 0) {
+                    if (max < count) max = count;
+                    count = 0;
+                }
+            }
+            if (count > max) max = count;
+        }
+        return max;
     }
 
     public int count(double[] array){
