3ce4bf9d45e2f496bc8ef47f8c9f81b42deb1874
Inna Osipova <rutheva_inna@mail.ru>
2022-10-21 21:25:37 +0300

    Task3

--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -2,13 +2,36 @@ package ru.gb.jseminar;
 
 public class Task3 {
 
-    //Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.
+    //Дан массив двоичных чисел, например [1,1,0,1,1,1,], вывести максимальное количество подряд идущих 1.
     public static void main(String[] args) {
+        double [] ar = new double []{1,1,0,1,1,1,1,0};
+        int kol = count(ar);
+        System.out.printf(" максимальное количество подряд идущих 1 = %s", kol);
+
 
     }
 
-    public int count(double[] array){
+  
+    public static int count(double[] array){
+        int [] temp = new int [array.length - 1];
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+                if (array[i] == 1){
+                    temp[j]++;
+                   
+                }
+                else{
+                    j++;
+                }}
+        int max = temp[0];
+            
+            for (int k : temp) {
+                if (k > max){
+                    max = k;
 
-        return 0;
+                }
+            }
+
+        return max;
     }
 }
