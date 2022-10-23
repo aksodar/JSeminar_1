83db8acafd399de7fc4b4dda87ee35cb6de58f0b
Inna Osipova <rutheva_inna@mail.ru>
2022-10-23 07:44:36 +0300

    Task4

--- src/main/java/ru/gb/jseminar/Task4.java
+++ src/main/java/ru/gb/jseminar/Task4.java
@@ -1,5 +1,7 @@
 package ru.gb.jseminar;
 
+import java.util.Scanner;
+
 public class Task4 {
 
     //Реализовать функцию возведения числа а в степень b. a, b – целые числа.
@@ -8,10 +10,19 @@ public class Task4 {
     //Пример 2: а = 2, b = -2, ответ: 0.25
     //Пример 3: а = 3, b = 0, ответ: 1
     public static void main(String[] args) {
+        Task4 t = new Task4();
+        Scanner iScaner = new Scanner (System.in);
+        System.out.print("Введите число : ");
+        int number = iScaner.nextInt();
+        System.out.print("Введите степень числа : ");
+        int degree = iScaner.nextInt();
+        iScaner.close();
+        System.out.printf("%d ^ %d = %s", number, degree, t.exponentiation(number, degree));
 
     }
 
     public double exponentiation(int a, int b){
-        return 0;
+
+        return Math.pow (a,b); 
     }
 }
