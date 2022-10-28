c3869b244280a897b0f92f5ca45d9472da351d09
YuliyaStuchilova <yulia.s9314@gmail.com>
2022-10-28 22:21:54 +0300

    add Tasks

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,5 +1,9 @@
 package ru.gb.jseminar;
 
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class Task2 {
 
     //В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида
@@ -8,7 +12,24 @@ public class Task2 {
     //"Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
     //"Доброй ночи, <Имя>!", если время от 23:00 до 4:59
     public static void main(String[] args) {
-
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите имя: ");
+        String name = iScanner.nextLine();
+        LocalDateTime ldt = LocalDateTime.now();
+        Integer time = ldt.getHour();
+        if (time >=5 && time<12){
+            System.out.printf("Доброе утро, %s", name);
+        }
+        else if (time >=12 && time<18){
+            System.out.printf("Добрый день, %s", name);
+        }
+        else if (time >=18 && time<23){
+            System.out.printf("Добрый вечер, %s", name);
+        }
+        else if (time >=23 && time<5){
+            System.out.printf("Доброй ночи, %s", name);
+        }
+        iScanner.close();
     }
 
 }
--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -4,11 +4,30 @@ public class Task3 {
 
     //Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.
     public static void main(String[] args) {
+        Task3 task3 = new Task3();
+        int[] arr = new int[]{1, 1, 0, 1, 1, 1, 0, 0, 1};
+        int result = task3.count(arr);
+        System.out.println(result);
 
     }
 
-    public int count(double[] array){
+    public int count(int[] array) {
+        int count = 0;
+        int max = 0;
+        for (int item : array) {
+            if (item == 1) {
+                count++;
+            } else {
+                if (count > max) {
+                    max = count;
+                }
+                count = 0;
+            }
+        }
+        if (count > max) {
+            max = count;
 
-        return 0;
+        }
+        return max;
     }
 }
